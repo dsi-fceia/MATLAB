42710f659eeb8746cd4f09b538e93ea788d91296
pista86 <pista86@gmail.com>
2015-10-23 21:09:57 -0300

    Se agregan archivos wav con gran variedad de tonos

--- AudioGenNTones.m
+++ AudioGenNTones.m
@@ -3,9 +3,9 @@
 %Salida: audioOut.wav
 %--------------------------------------------------------------------------
 %outFileName='./Audio/Procesado/audioOut!.wav';
-[FileName,PathName] = uigetfile('*.*','Seleccionar archivo de audio de salida');
+[FileName,PathName] = uiputfile('*.*','Seleccionar archivo de audio de salida');
 outFileName=strcat(PathName,FileName);
-toneFreqs = [50,1000]; %Vector de n tonos agreagados al archivo de entrada
+toneFreqs = [50,100,200,600,800,900,950,1000,1050,1100,1200,2000,4000,5000]; %Vector de n tonos agreagados al archivo de entrada
 Duration = 5;
 OUTNbits=16; %16 bits
 OUTFs = 16000;      
